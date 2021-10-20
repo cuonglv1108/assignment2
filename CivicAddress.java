aeac983d2d9faac452985c7d436078e432026bbc
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2021-10-20 12:47:08 -0400

    Update CivicAddress.java

--- CivicAddress.java
+++ CivicAddress.java
@@ -1,38 +1,50 @@
 package Two;
 
-    public class CivicAddress extends Address{
-        private String civicAddress;
-        public enum corresponding{
-            AB, BC, MB, NB, NL, NT, NS, NU, ON,
-            PE, QC, SK, YT
-        }
-        public corresponding code;
-        public CivicAddress() {
-            civicAddress = "NULL";
-            code = corresponding.AB;
-        }
-        public CivicAddress(String e, corresponding code) {
-            civicAddress = e;
-            this.code = code;
-        }
-        public CivicAddress(CivicAddress e) {
-            civicAddress = e.civicAddress;
-        }
+public class CivicAddress extends Address{
+    private String civicAddress;
+    public enum corresponding{
+        AB, BC, MB, NB, NL, NT, NS, NU, ON,
+        PE, QC, SK, YT
+    }
+    public corresponding code;
+    public String postal_code;
+    public CivicAddress() {
+        civicAddress = "NULL";
+        code = corresponding.AB;
+        postal_code = "NULL";
 
-        public corresponding getCode() {
-            return code;
-        }
-
-        public void setCode(corresponding code) {
-            this.code = code;
-        }
-
-        @Override
-        public String toString() {
-            return "CivicAddress{" +
-                    "civicAddress='" + civicAddress + '\'' +
-                    ", code=" + code +
-                    '}';
-        }
+    }
+    public CivicAddress(String e,corresponding code, String postal_code) {
+        civicAddress = e;
+        this.code = code;
+        this.postal_code = postal_code;
+    }
+    public CivicAddress(CivicAddress e) {
+        civicAddress = e.civicAddress;
     }
 
+    public corresponding getCode() {
+        return code;
+    }
+
+    public void setCode(corresponding code) {
+        this.code = code;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    @Override
+    public String toString() {
+        return "CivicAddress{" +
+                "civicAddress='" + civicAddress + '\'' +
+                ", code=" + code +
+                ", postal_code='" + postal_code + '\'' +
+                '}';
+    }
+}
