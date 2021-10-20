9be36cb0fc0121813e30c7e7452678ed6ad55c5a
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2021-10-20 12:46:18 -0400

    Update Vaccine.java

--- Vaccine.java
+++ Vaccine.java
@@ -1,45 +1,39 @@
 package One;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import Two.*;
-
-import java.util.Objects;
 
 
 public class Vaccine {
     public enum type_vaccine {
         Moderna, Pfizer, AstraZeneca
     }
-    protected Date day1;
-    protected Date day2;
+    protected static String day1;
+    protected static String day2;
     private type_vaccine type;
 
     public Vaccine() {
     }
 
     public Vaccine(String day1, String day2, type_vaccine type) {
-        this.day1 = convertDate(day1);
-        this.day2 = convertDate(day2);
+        this.day1 = day1;
+        this.day2 = day2;
         this.type = type;
     }
 
     public String getDay1() {
-        return convertDate(day1);
+        return day1;
     }
 
     public void setDay1(String day1) {
-        this.day1 = convertDate(day1);
+        this.day1 = day1;
     }
 
     public String getDay2() {
-        return convertDate(day2);
+        return day2;
     }
 
     public void setDay2(String day2) {
-        this.day2 = convertDate(day2);
+        this.day2 = day2;
     }
 
     public type_vaccine getType() {
@@ -50,37 +44,30 @@ public class Vaccine {
         this.type = type;
     }
 
-    public Date convertDate (String date) {
-        try {
-            //      String date = "07/28/2011";
-            Date date1 = new SimpleDateFormat("MM/dd/yyyy").parse(date);
-            return date1;
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return new Date();
-    }
-    public String convertDate (Date date){
-        String[] date2 = date.toLocaleString().split(",");
-       // System.out.println(date2[0] + "," + date2[1]);
-        return date2[0] + "," + date2 [1];
-    }
+    
 
     public String toString() {
-        return "Dose1: " +  type + ", "+  getDay1() + getDay2() + ", type=" + type + '}';
+        return "Dose1: " +  type + ", " +  getDay1() + " " + getDay2() + ", type=" + type ;
+    }
+
+    public static int set_time(int year, int month, int day){
+        if (month < 3) {
+            year--;
+            month += 12;
+        }
+        return 365*year + year/4 - year/100 + year/400 + (153*month - 457)/5 + day - 306;
     }
 
     public static void Clinic(){
-        CivicAddress c = new CivicAddress("8680 rue Saint Denis", CivicAddress.corresponding.AB);
+        CivicAddress c = new CivicAddress("8680 rue Saint Denis", CivicAddress.corresponding.AB,"H2P2H2");
         System.out.println(c.toString());
         Address test =  new Address("cuong","cuonglv","0985001998", c);
-        WebpageAddress test1 = new WebpageAddress("www.healthcarecentre.com");
+        WebPageAddress test1 = new WebPageAddress("www.healthcarecentre.com");
         EmailAddress test2 = new EmailAddress("heathcarecentre@gmail.com");
         TelecomAddress test3 = new TelecomAddress("0912343223");
-     //   System.out.println(test1.toString());
-      //  System.out.println(test2.toString());
-      //  System.out.println(test3);
+        //   System.out.println(test1.toString());
+        //  System.out.println(test2.toString());
+        //  System.out.println(test3);
         System.out.println(test.toString());
     }
-
 }
