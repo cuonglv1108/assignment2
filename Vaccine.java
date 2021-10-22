079d6204caa491fbeb9d747d89bf84dd9da0fc06
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2021-10-22 11:53:06 -0400

    Update Vaccine.java

--- Vaccine.java
+++ Vaccine.java
@@ -2,22 +2,38 @@ package One;
 
 import Two.*;
 
+import java.sql.SQLOutput;
+
 
 public class Vaccine {
     public enum type_vaccine {
         Moderna, Pfizer, AstraZeneca
     }
+    public enum clinic_address{
+        Stadium1, Stadium2, Stadium3, Stadium4
+    }
     protected static String day1;
     protected static String day2;
     private type_vaccine type;
+    private clinic_address cl_a;
 
     public Vaccine() {
+
     }
 
-    public Vaccine(String day1, String day2, type_vaccine type) {
+    public Vaccine(String day1, String day2, type_vaccine type, clinic_address cl_a) {
         this.day1 = day1;
         this.day2 = day2;
         this.type = type;
+        this.cl_a = cl_a;
+    }
+
+    public clinic_address getCl_a() {
+        return cl_a;
+    }
+
+    public void setCl_a(clinic_address cl_a) {
+        this.cl_a = cl_a;
     }
 
     public String getDay1() {
@@ -44,10 +60,14 @@ public class Vaccine {
         this.type = type;
     }
 
-    
 
-    public String toString() {
-        return "Dose1: " +  type + ", " +  getDay1() + " " + getDay2() + ", type=" + type ;
+
+    public String toString1() {
+        return "Dose1: " +  type + ", " +  getDay1() ;
+    }
+
+    public String toString2() {
+        return "Dose2: " +  type + ", " + getDay2() ;
     }
 
     public static int set_time(int year, int month, int day){
@@ -58,16 +78,16 @@ public class Vaccine {
         return 365*year + year/4 - year/100 + year/400 + (153*month - 457)/5 + day - 306;
     }
 
-    public static void Clinic(){
-        CivicAddress c = new CivicAddress("8680 rue Saint Denis", CivicAddress.corresponding.AB,"H2P2H2");
-        System.out.println(c.toString());
-        Address test =  new Address("cuong","cuonglv","0985001998", c);
-        WebPageAddress test1 = new WebPageAddress("www.healthcarecentre.com");
-        EmailAddress test2 = new EmailAddress("heathcarecentre@gmail.com");
-        TelecomAddress test3 = new TelecomAddress("0912343223");
-        //   System.out.println(test1.toString());
-        //  System.out.println(test2.toString());
-        //  System.out.println(test3);
-        System.out.println(test.toString());
+    public static void Clinic1(){
+        CivicAddress c = new CivicAddress("8680 rue Saint Denis ST.Montreal", CivicAddress.corresponding.AB,"H2P2H2");
+        Address test =  new Address("https:clinicaddress.com","levietcuongdn98@gmail.com","0985001998", c);
+        System.out.println("Clinic: " + clinic_address.Stadium1 + " " + c.toString());
+        System.out.println(test);
+    }
+    public static void Clinic2(){
+        CivicAddress c = new CivicAddress("8680 rue Saint Denis ST.Montreal", CivicAddress.corresponding.QC,"H2P2H2");
+        Address test =  new Address("https:clinicaddress.com","levietcuongdn98@gmail.com","0985001998", c);
+        System.out.println("Clinic: " + clinic_address.Stadium2 + " " + c.toString());
+        System.out.println(test);
     }
 }
