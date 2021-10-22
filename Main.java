21f099c7649b78f66e07429d2e7151cef4219dc1
cuonglv1108 <81017208+cuonglv1108@users.noreply.github.com>
2021-10-22 11:53:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import One.Patient;
 import One.Vaccine;
 import Two.Address;
 
+import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.Scanner;
 import java.text.DateFormat;
@@ -16,13 +17,17 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         welcome();
-        //  Patient p1 = new Patient("Cuong", 23, "levietcuongdn98@gmail.com", "0985001998", null);
-        //  Patient p2 = new Patient("Cuong", 23, "levietcuongdn98@gmail.com", "0985001998", null);
-        Vaccine v1 = new Vaccine("May 5, 2021", "August 6,2021", Vaccine.type_vaccine.Pfizer);
-     //   Vaccine.DateDiff();
+        Patient p1 = new Patient("Cuong", 23, "levietcuongdn98@gmail.com", "0985001998", null);
+        Patient p2 = new Patient("Cuong", 23, "levietcuongdn98@gmail.com", "0985001998", null);
+        Vaccine v1 = new Vaccine("May 5, 2021", "August 6, 2021", Vaccine.type_vaccine.Pfizer, Vaccine.clinic_address.Stadium3);
+        Vaccine v2 = new Vaccine("May 5, 2021", "August 6, 2021", Vaccine.type_vaccine.Pfizer, Vaccine.clinic_address.Stadium2);
+        System.out.println(p1);
+        System.out.println(v1.toString1());
+        Vaccine.Clinic1();
+        System.out.println(p2);
+        System.out.println(v1.toString2());
+        Vaccine.Clinic2();
         changetime();
-        System.out.println(v1);
-        Vaccine.Clinic();
     }
 
 
@@ -36,28 +41,33 @@ public class Main {
         String t = scanner.nextLine();
         int kq1 = 1;
         do{
-        if (t.equals("YES")) {
-            int year=0,year1=0,month=0,month1=0,day=0,day1=0;
-            int kq = 0;
-            System.out.println("Day for 1st dose: ");
-            year = scanner.nextInt();
-            month = scanner.nextInt();
-            day = scanner.nextInt();
-            System.out.println("Day for 2nd dose: ");
-            year1 = scanner.nextInt();
-            month1 = scanner.nextInt();
-            day1 = scanner.nextInt();
-            kq = Vaccine.set_time(year1,month1,day1)-Vaccine.set_time(year,month,day);
-            if (kq>28 && kq<=256) System.out.println("Valid day");
-            else System.out.println("Invalid day");
-            String day_1 = monthly[month-1] + " " + String.valueOf(day) + ", " + String.valueOf(year);
-            String day_2 = monthly[month1-1] + " " + String.valueOf(day1) + ", " + String.valueOf(year1);
-            Vaccine v1 = new Vaccine(day_1,day_2, Vaccine.type_vaccine.Pfizer);
-
-      //      System.out.println(day_1);
-      //      System.out.println(kq);
-        } else
-            System.out.println("DO YOU WANT TO CHANGE AGAIN? ");
+            if (t.equals("YES")) {
+                int year=0,year1=0,month=0,month1=0,day=0,day1=0;
+                int kq = 0;
+                System.out.println("Day for 1st dose: ");
+                year = scanner.nextInt();
+                month = scanner.nextInt();
+                day = scanner.nextInt();
+                System.out.println("Day for 2nd dose: ");
+                year1 = scanner.nextInt();
+                month1 = scanner.nextInt();
+                day1 = scanner.nextInt();
+                kq = Vaccine.set_time(year1,month1,day1)-Vaccine.set_time(year,month,day);
+                if (kq>28 && kq<=356) {
+                    System.out.println("Valid day");
+                    String day_1 = monthly[month - 1] + " " + String.valueOf(day) + ", " + String.valueOf(year);
+                    String day_2 = monthly[month1 - 1] + " " + String.valueOf(day1) + ", " + String.valueOf(year1);
+                    Vaccine v1 = new Vaccine(day_1, day_2, Vaccine.type_vaccine.Pfizer, Vaccine.clinic_address.Stadium3);
+                    Vaccine v2 = new Vaccine(day_1, day_2, Vaccine.type_vaccine.Pfizer, Vaccine.clinic_address.Stadium2);
+                    System.out.println(v1.toString1());
+                    Vaccine.Clinic1();
+                    System.out.println(v1.toString2());
+                    Vaccine.Clinic2();
+                }
+                else System.out.println("Invalid day");
+                break;
+            } else
+                System.out.println("DO YOU WANT TO CHANGE AGAIN? ");
             t = scanner.nextLine();
             if (t.equals("NO")){
                 break;
